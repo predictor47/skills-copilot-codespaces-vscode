a5086e6f58e765cf12812f2cfbde4ba8c89e3bbf
Muneeb-ur-Rehman <80532885+predictor47@users.noreply.github.com>
2025-11-28 08:22:58 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!\n');
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
